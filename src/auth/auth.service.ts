987e898ed83023bda12f5f7eae3874566077d1a5
Eduardo Sandoval <edsandoval.dev@gmail.com>
2026-05-22 17:21:55 -0600

    Se crea la verificación del correo y la contraseña para el feature de login

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,12 +1,13 @@
 import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
-import { BadRequestException, Injectable, InternalServerErrorException } from '@nestjs/common';
+import { BadRequestException, Injectable, InternalServerErrorException, UnauthorizedException } from '@nestjs/common';
 
 import { InjectModel } from '@nestjs/mongoose';
 import { Model } from 'mongoose';
 import * as bcryptjs from 'bcryptjs';
 
 import { User } from './entities/user.entity';
+import { LoginDto } from './dto/login.dto';
 
 @Injectable()
 export class AuthService {
@@ -41,6 +42,27 @@ export class AuthService {
     }
 
 
+  }
+
+  async login(loginDto: LoginDto) {
+    const { email, password } = loginDto;
+
+    const user = await this.userModel.findOne( {email} );
+
+    if(!user) {
+      throw new UnauthorizedException("Not valid credentials - email");
+    }
+
+    if(!bcryptjs.compareSync( password!, user.password! )) {
+      throw new UnauthorizedException("Not valid credentials - password");
+    }
+
+    const { password:_, ...rest } = user.toJSON();
+
+    return {
+      user: rest,
+      token: 'ABC-123'
+    }
   }
 
   findAll() {
