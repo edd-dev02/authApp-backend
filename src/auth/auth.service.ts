ab2bfc278db05dc145ad666e0611ed9f92485a80
Eduardo Sandoval <edsandoval.dev@gmail.com>
2026-05-21 21:54:28 -0600

    Se crea el método para crear un usuario con contraseña encriptada y devolver la data necesaria

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,12 +1,46 @@
-import { Injectable } from '@nestjs/common';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
+import { BadRequestException, Injectable, InternalServerErrorException } from '@nestjs/common';
+
+import { InjectModel } from '@nestjs/mongoose';
+import { Model } from 'mongoose';
+import * as bcryptjs from 'bcryptjs';
+
+import { User } from './entities/user.entity';
 
 @Injectable()
 export class AuthService {
-  create(createUserDto: CreateUserDto) {
-    console.log(createUserDto);
-    return 'This action adds a new auth';
+
+  constructor(
+    @InjectModel(User.name) private userModel: Model<User>,
+  ) { }
+
+  async create(createUserDto: CreateUserDto): Promise<User> {
+
+    //return newUser.save();
+
+    try {
+      const { password, ...userData } = createUserDto;
+      
+      const newUser = new this.userModel({
+        password: bcryptjs.hashSync( password!, 10 ),
+        ...userData
+      });
+      
+      await newUser.save();
+      const { password: _, ...user } = newUser.toJSON();
+
+      return user
+
+    } catch (error: any) {
+      if (error.code === 11000) {
+        throw new BadRequestException(`${createUserDto.email} already exists!`);
+      }
+
+      throw new InternalServerErrorException("Something terrible happened!")
+    }
+
+
   }
 
   findAll() {
